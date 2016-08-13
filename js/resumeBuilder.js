4762e71f7ffab47b9bb269af0a820795e515f0f8
Matt Hardwick <matthardwick@gmail.com>
2016-08-12 17:58:46 -0700

    building/testing resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,59 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Matthew Hardwick";
+var role = "Front-End Web Developer";
+var contactInfo = "matthardwick@gmail.com";
+var bioPic = "images/fry.jpg";
+var welcomeMessage = "Hello!";
+var skills = ["a", "b", "c"];
+
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
+
+
+var bio = {
+  "name" : name,
+  "role" : role,
+  "contacts" : {
+    "email" : "matthardwick@gmail.com",
+    "github" : "matthardwick",
+  },
+  "bioPic" : bioPic,
+  "welcomeMessage" : welcomeMessage,
+  "skills" : skills
+};
+
+
+var work = {};
+work.position = "CSA";
+work.employer = "DMIG";
+work.years =  3;
+work.city = "Fresno";
+
+
+var education = {};
+education["name"] = "Reedley";
+education["years"] = 3;
+
+
+$("#main").append(work["position"]);
+$("#main").append(education.name);
+
+var education = {
+  "schools": [
+    {
+      "name": "Reedley",
+      "city": "Reedley, CA",
+      "degree": "Piano"
+    }
+  ],
+  "OnlineCourses": [
+    {
+      "name": "Udacity",
+      "course": "NanoDegree"
+    }
+  ]
+}
