4967f8d19c2f91ad98f5b054683fc451bfe1ae70
Matt Hardwick <matthardwick@gmail.com>
2016-08-16 08:47:26 -0700

    upload completed r1
    changed pc, git wasn't working so having to upload one giant commit

--- js/helper.js
+++ js/helper.js
@@ -1,9 +1,6 @@
 /*
-
 This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
 Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
 Cameron Pittman
 */
 
@@ -13,7 +10,7 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span class="role">%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -26,7 +23,7 @@ var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</sp
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
@@ -60,37 +57,34 @@ var googleMap = '<div id="map"></div>';
 
 
 /*
-The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
+The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
-$(document).ready(function() {
-  $('button').click(function() {
-    var $name = $('#name');
-    var iName = inName($name.text()) || function(){};
-    $name.html(iName);
-  });
-});
+// $(document).ready(function() {
+//   $('button').click(function() {
+//     var $name = $('#name');
+//     var iName = inName($name.text()) || function(){};
+//     $name.html(iName);
+//   });
+// });
 
 /*
-The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
+The next few lines about clicks are for the Collecting Click Locations quiz in the lesson Flow Control from JavaScript Basics.
 */
-var clickLocations = [];
-
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      x: x,
-      y: y
-    }
-  );
-  console.log('x location: ' + x + '; y location: ' + y);
-}
-
-$(document).click(function(loc) {
-  var x = loc.pageX;
-  var y = loc.pageY;
-
-  logClicks(x,y);
-});
+// var clickLocations = [];
+//
+// function logClicks(x,y) {
+//   clickLocations.push(
+//     {
+//       x: x,
+//       y: y
+//     }
+//   );
+//   console.log('x location: ' + x + '; y location: ' + y);
+// }
+//
+// $(document).click(function(loc) {
+//   // your code goes here!
+// });
 
 
 
@@ -129,16 +123,18 @@ function initializeMap() {
     // initializes an empty array
     var locations = [];
 
-    // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
+    // changed to iterate through contacts since contacts were put in an array
+    bio.contacts.forEach(function(contact){
+      locations.push(contact.location);
+    });
 
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
     // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    // education.schools.forEach(function(school){
-    //   locations.push(school.location);
-    // });
+    education.schools.forEach(function(school){
+      locations.push(school.location);
+    });
 
     // iterates through work locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
@@ -243,10 +239,10 @@ Uncomment the code below when you're ready to implement a Google Map!
 
 // Calls the initializeMap() function when the page loads
 window.addEventListener('load', initializeMap);
-//
+
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
 window.addEventListener('resize', function(e) {
-//   Make sure the map bounds get updated on page resize
+  // Make sure the map bounds get updated on page resize
  map.fitBounds(mapBounds);
 });
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,113 +1,218 @@
+// Resume data
 var bio = {
   "name": "Matthew Hardwick",
   "role": "Front-End Web Developer",
-  "contacts": {
+  "contacts": [{
+    "mobile": "559-549-7123",
     "email": "matthardwick@gmail.com",
     "github": "matthardwick",
     "location": "Fresno, CA"
-  },
-  "welcomeMessage": "Hello World!",
+  }],
+  "welcomeMessage": "Welcome! Thank you for taking a look at my Online Résumé",
   "skills": [
-    "a", "b", "c"
+    "HTML, CSS, Javascript", "Professional Logo/Image Vectorization (Illustrator)", "Microsoft Excel (Advanced)"
   ],
-  "bioPic": "images/fry.jpg"
-}
+  "bioPic": "images/avatar.jpg"
+};
+
+var education = {
+  "schools": [{
+    "name": "Reedley College",
+    "location": "995 North Reed Ave, Reedley, CA 93654",
+    "degree": "BA",
+    "majors": "Music (Instrumental)",
+    "dates": "August 2009 - June 2012",
+    "url": "reedleycollege.edu"
+  }],
+  "onlineCourses": [{
+    "title": "Front End Web Developer NanoDegree",
+    "school": "Udacity",
+    "dates": "May 2016 - In Progress",
+    "url": "http://www.udacity.com"
+  }]
+};
 
 var work = {
   "jobs": [{
     "employer": "DerManouel Insurance Group",
     "title": "Account Manager",
-    "location": "Fresno, CA",
-    "dates": "April 2013 to Present",
-    "description": "Managing a book of dedicated Health Insurance business with responsibilies including Marketing, Enrollment, Escalated Claims Issues, and developing tools to ease the transition into the Affordable Care Act rating system",
+    "location": "548 W. Cromwell Ave., Fresno, CA 93711",
+    "dates": "April 2013 - September 2016",
+    "description": "Managing a book of dedicated Health Insurance business with responsibilies including Marketing, Enrollment, Escalated Claims Issues, and developing tools to ease the transition into the Affordable Care Act Small Group rating system",
     "license": "California Life and Accident/Health Agent - 0I08142"
   }, {
     "employer": "Aetna",
     "title": "Customer Service Representative",
-    "location": "Fresno, CA",
-    "dates": "January 2008 to April 2013",
-    "description": "Incoming call center",
+    "location": "1385 E Shaw Ave, Fresno, CA 93710",
+    "dates": "January 2008 - April 2013",
+    "description": "Taking calls from an inbound queue from members, providers, plan sponsors, and brokers.</br> Assigned to the 'mentor queue,' taking calls from other customer service representatives and handling supervisor requests.</br> Monitoring the 'Fresno Small Group Mailbox,' an email box designed to answer questions from brokers, general agents, and Sales Executives regarding escalated claim and enrollment issues.</br> Regional Business Sales Support Appreciation Award for 2012. Recognized for performing excellent service and collaborative efforts to Aetna's Sales team.",
+  }, {
+    "employer": "Alpha Designs",
+    "title": "Graphics Designer",
+    "location": "316 E 4th St, Hanford, CA 93230",
+    "dates": "April 2004 - June 2006",
+    "description": "Managed orders from customers, both in person and over the phone.</br> Developed graphics solutions for silk-screen, embroidery, signs and print media.</br> Installed signs and vinyl graphics.</br> Experience in daily use of graphics software such as Adobe Photoshop and Illustrator.",
   }]
-}
+};
 
 var projects = {
-  "websites": [{
+  "projects": [{
     "title": "Where the Garden Grows Preschool",
-    "url": "wtggpreschool.com"
+    "dates": "May 2016",
+    "description": "Basic multi-page website for client. Hosted on a VPS (DigitalOcean, Ubuntu 14.04) using NGINX as the web-server. Built with GRAV as the CMS for the client using a modified Gravstrap theme. Mobile ready and responsive. Everything was configured from scratch on an extremely tight deadline.",
+    "images": [
+      "images/wtgg-thumb2.png", "images/wtgg-thumb.png"
+    ],
+    "url": "https://github.com/matthardwick/wtgg"
   }, {
     "title": "Personal Blog",
-    "url": "mattdhardwick.com"
+    "dates": "June 2016",
+    "description": "personal responsive blog using Jekyll, hosted on github-pages.",
+    "images": [
+      "images/blog.PNG"
+    ],
+    "url": "https://github.com/matthardwick/matthardwick.github.io"
+  }, {
+    "title": "Portfolio Page",
+    "dates": "July 2016",
+    "description": "Portolio Page built on Google's Web Starter Kit",
+    "images": [
+      "images/portfolio-image.PNG",
+      "images/portfolio-image2.PNG"
+    ],
+    "url": "https://github.com/matthardwick/lesson-portfolio"
+  }, {
+    "title": "Animal Trading Card Demo",
+    "dates": "May 2016",
+    "description": "CSS card demo",
+    "images": [
+      "images/coyote-css-card.PNG"
+    ],
+    "url": "https://github.com/matthardwick/lesson-card-css"
   }]
-}
+};
+// End of Resume Data
+
+// Header Containing bio section: role, name, contacts, bioPic, welcomeMessage, skills
+bio.display = function () {
+  var formattedName = HTMLheaderName.replace("%data%", bio.name);
+  var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+  var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+  var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+  $("#header").prepend(formattedRole).prepend(formattedName).append(formattedBioPic, formattedWelcomeMsg, HTMLskillsStart);
+
+  // Iterate over skills and display each
+  for (skill in bio.skills) {
 
 
-var education = {
-  "Online Courses": [{
-    "name": "Udacity",
-    "type": "Front-End Web Developer NanoDegree"
-  }]
-}
+    var formattedSkills = HTMLskills.replace("%data%", bio.skills[skill]);
+    $("#header").append(formattedSkills);
+  }
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
+  // Iterate over contacts and display each
+  for (contact in bio.contacts) {
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts[contact].mobile);
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts[contact].email);
+    var formattedGithub = HTMLgithub.replace("%data%", bio.contacts[contact].github);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts[contact].location);
+    $("#topContacts:last").append(formattedMobile, formattedEmail, formattedGithub, formattedLocation);
+    $("#footerContacts:last").append(formattedMobile, formattedEmail, formattedGithub, formattedLocation);
+  }
+};
 
-
-if(bio.skills.length > 0) {
-  $("#header").append(HTMLskillsStart);
-
-  var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-  $("#skills").append(formattedSkill);
-  var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-  $("#skills").append(formattedSkill);
-  var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-  $("#skills").append(formattedSkill);
-}
-
-function displayWork() {
-  for(job in work.jobs) {
+// Body of HTML start - Work Experience Section
+work.display = function () {
+  for (job in work.jobs) {
     $("#workExperience").append(HTMLworkStart);
 
     var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
     var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-    var formattedEmployerTitle = formattedEmployer + formattedTitle;
-    $(".work-entry:last").append(formattedEmployerTitle);
-
+    var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+    var formattedWorkTitle = formattedEmployer + formattedTitle;
     var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-    $(".work-entry:last").append(formattedDates);
-
     var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-    $(".work-entry:last").append(formattedDescription);
+
+    $(".work-entry:last").append(formattedWorkTitle, formattedWorkLocation, formattedDates, formattedDescription);
   }
-}
+};
 
-displayWork();
-
-
-projects.display = function() {
-  for (project in projects.websites) {
+// Projects Section
+projects.display = function () {
+  for (project in projects.projects) {
     $("#projects").append(HTMLprojectStart);
 
-    var formattedTitle = HTMLprojectTitle.replace("%data%", projects.websites[project].title);
-    $(".project-entry:last").append(formattedTitle);
 
-    var formattedURL = HTMLprojectDescription.replace("%data%", projects.websites[project].url);
-    $(".project-entry:last").append(formattedURL);
+    // contained setting title within $ to chain setting each href as well
+    var formattedProjectTitle = $(HTMLprojectTitle.replace("%data%", projects.projects[project].title)).attr("href", projects.projects[project].url);
+    //
+    var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+    var formattedProjectDescr = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+
+    $(".project-entry:last").append(formattedProjectTitle)
+
+    $(".project-entry:last").append(formattedProjectDates, formattedProjectDescr);
+
+    // iterate over images in project images and display
+    // each in their relative project
+    for (image in projects.projects[project].images) {
+      var formattedProjectImages = $(HTMLprojectImage.replace("%data%", projects.projects[project].images[image])).addClass("project-image");
+      $(".project-entry:last").append(formattedProjectImages);
+    }
+    $(".project-entry:last").append("<hr>");
   }
-}
+
+};
+
+// Education Section
+education.display = function () {
+  // Schools
+  for (school in education.schools) {
+    $("#education").append(HTMLschoolStart);
+
+    var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+    var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+    var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+    var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+    var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+    var formattedSchoolNameDegree = formattedSchoolName + formattedSchoolDegree;
+    var linkedSchoolName = $(formattedSchoolNameDegree).attr("href", education.schools[school].url);
+    //
+    $(".education-entry:last").append(linkedSchoolName, formattedSchoolLocation, formattedSchoolDates, formattedSchoolMajor);
+  }
+
+  // Online Courses
+  for (course in education.onlineCourses) {
+    $("#education").append(HTMLonlineClasses, HTMLschoolStart);
+
+    var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+    var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+    var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
+    var formattedOnlineURL = $(HTMLonlineURL.replace("%data%", education.onlineCourses[course].url)).attr("href", education.onlineCourses[course].url);
+    var formattedOnlineSchoolTitle = formattedOnlineTitle + formattedOnlineSchool;
+    var linkedCourseName = $(formattedOnlineSchoolTitle).attr("href", education.onlineCourses[course].url);
+
+    $(".education-entry:last").append(linkedCourseName, formattedOnlineDates, formattedOnlineURL);
+  }
+};
+
+// display each section
+work.display();
 projects.display();
+education.display();
+bio.display();
 
-
+// display the map
 $("#mapDiv").append(googleMap);
 
-$("#main").append(internationalizeButton);
 
-function inName(name) {
-  name = name.trim().split(" ");
-  console.log(name);
-  name[1] = name[1].toUpperCase();
-  name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
-
-  return name[0] + " " + name[1];
-}
+// $("#main").append(internationalizeButton);
+//
+// function inName(name) {
+//   name = name.trim().split(" ");
+//   console.log(name);
+//   name[1] = name[1].toUpperCase();
+//   name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+//
+//   return name[0] + " " + name[1];
+// }
